0fa6f8cbd033893b3eaba9be8a77e5cfcb006c35
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-01 23:13:21 -0400

    Added Signin/signup and styled navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,11 +5,24 @@ import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
   return (
-    <div className="gpt3__navbar">
-        <div className="gpt3__navbar-links">
-            <div className="gpt3__navbar-links-logo">
+    <div className="gpt__navbar">
+        <div className="gpt__navbar-links">
+            <div className="gpt__navbar-links-logo">
                 <img src={logo} alt="logo" />
             </div>
+
+            <div className="gpt__navbar-links_container">
+              <p><a href="#home">Home</a></p>
+              <p><a href="#gpt">What is GPT3?</a></p>
+              <p><a href="#possibility">Open AI</a></p>
+              <p><a href="#features">Features</a></p>
+              <p><a href="#library">Library</a></p>
+            </div>
+        </div>
+
+        <div className="gpt__navbar-sign">
+            <p>Sign In</p>
+            <button type="button">Sign up</button>
         </div>
     </div>
   )
