bb1b9e5f458c085c84828f5294b3f4c54bf02ba7
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-03 13:32:17 -0400

    Imported images, pass img, date, title to Article and display

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './article.css';
 
-const Article = () => {
+const Article = ({imgUrl, date, title}) => {
   return (
-    <div>Article</div>
+    <div className="gpt__blog-container_article">
+        <div className="gpt__blog-container_article-img">
+            <img src={imgUrl} alt="Blog Graphic" />
+        </div>
+          <div className="gpt__blog-container_article-title">
+              <div>
+                <p>{date}</p>
+                <p>{title}</p>
+              </div>
+              <p>Read More...</p>
+          </div>
+    </div>
   )
 }
 
