f99352e4e492b94b7ebcf850a06190239dbfef66
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-03 12:14:44 -0400

    Added image, import image to possibility section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './possibility.css'
 
+import possibilityImage from '../../assets/possibility.png';
+
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className="gpt__possibility section__padding" id="possibility">
+        <div className="gpt__possibility-image">
+            <img src={possibilityImage} alt="Possibilities AI" />  
+        </div>
+    </div>
   )
 }
 
