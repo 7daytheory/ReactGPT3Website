640d3ecc7357f85c480a7bf2b0ad19e7e161ced6
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-03 12:18:20 -0400

    Added content to right side of section, styled section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -9,6 +9,13 @@ const Possibility = () => {
         <div className="gpt__possibility-image">
             <img src={possibilityImage} alt="Possibilities AI" />  
         </div>
+
+        <div className="gpt__possibility-content">
+          <h4>Request early access to get started.</h4>
+          <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+          <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+          <h4>Request early access to get started.</h4>
+        </div>
     </div>
   )
 }
